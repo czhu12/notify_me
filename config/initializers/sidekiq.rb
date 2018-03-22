33fd3c74110fbf9f2f6f6624f8e49eaa8421b7f9
Chris Zhu <chris.zhu12@gmail.com>
2018-03-21 23:53:05 -0700

    fixed client for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,7 @@
 if Rails.env.production?
+  Sidekiq.configure_client do |config|
+    config.redis = { url: 'redis://redis:6379/12' }
+  end
   Sidekiq.configure_server do |config|
     config.redis = { url: 'redis://redis:6379/12' }
   end
