3888be00aa04190ec59f93e5ca78fc9072ebebb1
Chris Zhu <chris.zhu12@gmail.com>
2018-07-05 01:17:14 -0700

    fixed redis port

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  REDIS = Redis.new(host: "redis", port: 6379, db: 0)
+  REDIS = Redis.new(host: "127.0.0.1", port: 6379, db: 0)
 elsif Rails.env.development?
   REDIS = Redis.new(host: "127.0.0.1", port: 6379, db: 0)
 else
