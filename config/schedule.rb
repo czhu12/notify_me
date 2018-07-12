41b2df4a81a781b84c61cc65da4b7471975e8ed7
Chris Zhu <chris.zhu12@gmail.com>
2018-07-11 22:11:40 -0700

    added ENV to whenever schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,7 @@
+env :PATH, ENV['PATH']
+env :GEM_PATH, ENV['GEM_PATH']
+
 set :output, '/home/deploy/notify_me/shared/log/whenever.log'
-every 5.minutes roles: [:web] do
+every 30.minutes roles: [:web] do
   rake "schedule_listeners:enqueue_requests"
 end
