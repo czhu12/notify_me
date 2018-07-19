c15b06e1c7283f123b2d5efbfa799954c7bea93d
Chris Zhu <chris.zhu12@gmail.com>
2018-07-18 23:52:31 -0700

    send initial text to user

--- app/workers/create_listener_worker.rb
+++ app/workers/create_listener_worker.rb
@@ -4,10 +4,24 @@ class CreateListenerWorker
 
   def perform(args)
     listener = get_listener(args['listener_id'])
-    AlertMailer.registered_listener(
-      listener,
-      {to: listener.email, subject: "Created new listener for #{listener.query}"},
-    ).deliver_now
+    if listener.email
+      AlertMailer.registered_listener(
+        listener,
+        {to: listener.email, subject: "Created new listener for #{listener.query}"},
+      ).deliver_now
+    end
+
+    if listener.phone_number
+      text_client = Plivo::RestClient.new(
+        Rails.application.secrets[:plivo_auth_id],
+        Rails.application.secrets[:plivo_auth_token],
+      )
+      text_client.messages.create(
+        Rails.application.secrets[:plivo_phone_number],
+        [listener.phone_number],
+        "Created new listener for #{listener.query} #{listener.permalink}",
+      )
+    end
   end
 
   def get_listener(listener_id)
